f1fc1cfe942df6282e3209e660d8790659d55d06
Lucas-GES <lucas.espiritosanto@edu.unifil.br>
2022-03-24 12:19:18 -0300

    Simple update with print inside methods

--- src/main/java/service/AccountService.java
+++ src/main/java/service/AccountService.java
@@ -13,12 +13,14 @@ public class AccountService implements ServiceAccount{
     public void deposit(double deposit) {
         double balance = acc.getBalance() + deposit;
         acc.setBalance(balance);
+        System.out.println("Executing Deposit...");
     }
 
     @Override
     @Transaction
     public void withdraw(double withdraw) {
         double balance = acc.getBalance();
+        System.out.println("Executing withdraw...");
         if(acc.getBalance() > 0 && acc.getBalance() <= withdraw){
             acc.setBalance(balance - withdraw);
         }else{
